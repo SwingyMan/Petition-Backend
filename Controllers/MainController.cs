e99979e0d30c7afa607c56316be16b97f320cb78
Marcin <asssinowo90@gmail.com>
2023-03-27 11:43:48 +0200

    remove template controller, add "/" endpoint

---
+++ Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class MainController : ControllerBase
+    {
+        // GET: MainController
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("It's working");
+        }
+    }
+}
